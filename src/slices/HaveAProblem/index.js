54c41fa8eb339288593347016d62ee586b6cfa97
Nusfinal <nus_final@hotmail.com>
2024-01-15 09:21:16 +0700

    Update problem section

--- src/slices/HaveAProblem/index.js
+++ src/slices/HaveAProblem/index.js
@@ -14,23 +14,37 @@ const HaveAProblem = ({ slice }) => {
   const haveAProblemMobile = data.have_a_problem_image_mobile;
 
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      <hr />
-      <h1>{haveAProblemTitle}</h1>
-      <PrismicImage field={haveAProblemDesktop} />
-
-      {items.map((item, index) => (
-        <div key={index}>
-          {item.button_title}
-          {item.button_color}
-          <PrismicImage field={item.have_a_problem_image_icon} />
-          <PrismicRichText field={item.have_a_problem_description} />
+    <>
+      <section className="container-divider"></section>
+      <div className="container-content problem py-[50px] lg:py-[80px]">
+        <h2>{haveAProblemTitle}</h2>
+      </div>
+      <div className="content-banner no-text"
+        data-slice-type={slice.slice_type}
+        data-slice-variation={slice.variation}
+      >
+        <div className="hidden md:block">
+          <PrismicImage field={haveAProblemDesktop} />
         </div>
-      ))}
-    </section>
+        <div className="block md:hidden">
+          <PrismicImage field={haveAProblemDesktop} />
+        </div>
+      </div>
+      <div className="container-content py-[50px] lg:py-[100px]">
+        <div className="content-with-icon">
+          <div className="content">
+            {items.map((item, index) => (
+              <div className="box-item" key={index}>
+                {/* {item.button_title}
+                {item.button_color} */}
+                <PrismicImage field={item.have_a_problem_image_icon} />
+                <PrismicRichText field={item.have_a_problem_description} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
