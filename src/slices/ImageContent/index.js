70d92cf2d6f7e5fb7859b7fb418ea9445971a7b7
supapong <supapong@deepscope.com>
2024-01-22 00:19:54 +0700

    update new slicezone

--- src/slices/ImageContent/index.js
+++ src/slices/ImageContent/index.js
@@ -1,18 +1,28 @@
+import { PrismicNextImage } from "@prismicio/next";
+import PropTypes from "prop-types";
 /**
  * @typedef {import("@prismicio/client").Content.ImageContentSlice} ImageContentSlice
  * @typedef {import("@prismicio/react").SliceComponentProps<ImageContentSlice>} ImageContentProps
  * @param {ImageContentProps}
  */
 const ImageContent = ({ slice }) => {
+  const data = slice.primary;
+  const imageDesktop = data.image_desktop;
+  const imageMobile = data.image_mobile;
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for image_content (variation: {slice.variation})
-      Slices
+      <hr />
+      <PrismicNextImage field={imageDesktop} alt="" />
+      <PrismicNextImage field={imageMobile} alt="" />
     </section>
   );
+};
+
+ImageContent.propTypes = {
+  slice: PropTypes.string,
 };
 
 export default ImageContent;
