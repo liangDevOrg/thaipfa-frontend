370b8edb38a4132d2beadbddb5005cabf6603d26
Supapong Engkasuwansiri <supapong_eng@yahoo.com>
2024-01-24 12:07:58 +0700

    query data from speakerwithcontent

--- src/slices/SpeakerWithContent/index.js
+++ src/slices/SpeakerWithContent/index.js
@@ -1,19 +1,52 @@
+import { PrismicRichText } from "@/components/PrismicRichText";
+import { PrismicNextImage } from "@prismicio/next";
+import PropTypes from "prop-types";
+
 /**
  * @typedef {import("@prismicio/client").Content.SpeakerWithContentSlice} SpeakerWithContentSlice
  * @typedef {import("@prismicio/react").SliceComponentProps<SpeakerWithContentSlice>} SpeakerWithContentProps
  * @param {SpeakerWithContentProps}
  */
 const SpeakerWithContent = ({ slice }) => {
-  
+  const data = slice.primary;
+  const imageLogo = data.image_logo;
+  const title = data.title;
+  const subtitle = data.subtitle;
+  const items = slice.items;
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
       <hr />
-      
+      <PrismicNextImage field={imageLogo} alt=""/>
+      <h1>{title}</h1>
+      <PrismicRichText field={subtitle} />
+
+      <div className="content-with-image speaker mt-[50px]">
+        <div className="content">
+          {items.map((item, index) => (
+            <div className="box-item" key={index}>
+              {/* <img alt="" src="../assets/images/icon-check.png"> */}
+              <div className="hidden md:block">
+                <PrismicNextImage field={item.speaker_image_desktop} alt="" />
+              </div>
+              <div className="block md:hidden">
+                <PrismicNextImage field={item.speaker_image_mobile} alt="" />
+              </div>
+              <h3>{item.speaker_name}</h3>
+              <PrismicRichText field={item.speaker_detail} />
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
+};
+
+SpeakerWithContent.propTypes = {
+  slice: PropTypes.string,
 };
 
 export default SpeakerWithContent;
