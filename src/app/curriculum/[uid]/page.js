cf0d1e4f611834d3f57bf70b6091779d07834250
supapong <supapong@deepscope.com>
2024-01-27 22:05:39 +0700

    refactor code query curriculum page

--- src/app/curriculum/[uid]/page.js
+++ src/app/curriculum/[uid]/page.js
@@ -1,10 +1,9 @@
 import { notFound } from "next/navigation";
-import { asText } from "@prismicio/client";
 import { SliceZone } from "@prismicio/react";
-
+import TitleAndSubTitle from "@/components/TitleAndSubTitle";
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
-import TitleAndSubTitle from "@Components/TitleAndSubTitle";
+
 /**
  * @typedef {{ uid: string }} Params
  */
@@ -21,7 +20,7 @@ export async function generateMetadata({ params }) {
   const settings = await client.getSingle("settings");
 
   return {
-    title: `${curriculum.data.title} | ${settings.data.siteTitle}`,
+    title: `${curriculum.data.meta_title} | ${curriculum.data.siteTitle}`,
     description: curriculum.data.meta_description,
     openGraph: {
       title: curriculum.data.meta_title,
@@ -34,33 +33,44 @@ export async function generateMetadata({ params }) {
   };
 }
 
+async function getData(params) {
+  const client = createClient();
+  const page = await client
+    .getByUID("curriculum", params.uid)
+    .catch(() => notFound());
+  return page;
+}
+
 /**
  * @param {{ params: Params }}
  */
 export default async function Curriculum({ params }) {
-  const client = createClient();
-  const curriculum = await client
-    .getByUID("curriculum", params.uid)
-    .catch(() => notFound());
+  try {
+    const curriculum = await getData(params);
 
-  return (
-    <>
-      <TitleAndSubTitle
-        title={curriculum.data.title}
-        subtitle={curriculum.data.subtitle}
-        themePage={curriculum.data.theme_page}
-      />
-      <SliceZone slices={curriculum.data.slices} components={components} />
-    </>
-  );
+    return (
+      <>
+        <TitleAndSubTitle
+          title={curriculum.data.title}
+          subtitle={curriculum.data.subtitle}
+          themePage={curriculum.data.theme_page}
+        />
+        <SliceZone slices={curriculum.data.slices} components={components} />
+      </>
+    );
+  } catch (err) {
+    const errorMessage = err.response?.data?.message || "Could not fetch item.";
+    console.error("Error fetching item:", errorMessage);
+    throw new Error(errorMessage);
+  }
 }
 
 export async function generateStaticParams() {
   const client = createClient();
 
-  const curriculums = await client.getAllByType("curriculum");
+  const allCurriculum = await client.getAllByType("curriculum");
 
-  return curriculums.map((curriculum) => {
-    return { uid: curriculum.uid };
+  return allCurriculum.map((page) => {
+    return { uid: page.uid };
   });
 }
