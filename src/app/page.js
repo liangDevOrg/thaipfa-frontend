7dfa69f34eb0ac6e07282bb798f6426df73648ea
Supapong Engkasuwansiri <supapong_eng@yahoo.com>
2024-01-30 15:53:57 +0700

    set auto route to homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,42 +1,9 @@
-import { notFound } from "next/navigation";
-import { SliceZone } from "@prismicio/react";
-import { createClient } from "@/prismicio";
-import { components } from "@/slices";
+"use client";
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
-/**
- * @returns {Promise<import("next").Metadata>}
- */
-export async function generateMetadata() {
-  const client = createClient();
-  const page = await client.getByUID("home", "home");
-
-  return {
-    title: page.data.meta_title,
-    description: page.data.meta_description,
-    openGraph: {
-      title: page.data.meta_title,
-      images: [
-        {
-          url: page.data.meta_image.url,
-        },
-      ],
-    },
-  };
-}
-
-async function getData() {
-  const client = createClient();
-  const page = await client.getByUID("home", "home").catch(() => notFound());
-  return page;
-}
-
-export default async function Homepage() {
-  try {
-    const homePage = await getData();
-    return <SliceZone slices={homePage.data.slices} components={components} />;
-  } catch (err) {
-    const errorMessage = err.response?.data?.message || "Could not fetch item.";
-    console.error("Error fetching item:", errorMessage);
-    throw new Error(errorMessage);
-  }
+export default function Page() {
+  useEffect(() => {
+    redirect("/home");
+  }, []);
 }
